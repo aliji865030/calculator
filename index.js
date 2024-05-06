772f0cc01056486d126a565b744274ff09c859c4
aliji865030 <abbasalichand786@gmail.com>
2024-05-06 22:38:15 +0530

    files added

---
+++ index.js
@@ -0,0 +1,17 @@
+const add=(a,b)=>a+b;
+
+const sub=(a,b)=>a-b;
+
+const mul=(a,b)=>a*b;
+
+const div=(a,b)=>a/b;
+
+
+const calculator={
+    add,
+    sub,
+    mul,
+    div
+}
+
+module.exports=calculator;
